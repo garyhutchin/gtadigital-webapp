946bb7d84a6122959b22168005bdfdbc3c5fad02
Gary Hutchin <garyscotthutchin@gmail.com>
2020-08-25 19:37:40 +0100

    Further updates to release-details
    Audio assets added
    
    GTA01 details fully populated.
    
    All route methods created and corresponding click events added within release-list.component

--- src/app/music/releases/release-details/gta001/gta001.component.ts
+++ src/app/music/releases/release-details/gta001/gta001.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Track } from 'ngx-audio-player';
 
 @Component({
   selector: 'gta1-details',
-  templateUrl: './gta001.component.html',
-  styleUrls: ['./gta001.component.css']
+  templateUrl: 'gta001.component.html',
+  styleUrls: ['gta001.component.css']
 })
-export class GTA1Component implements OnInit {
+export class GTA1Component {
 
-  constructor() { }
+    // Material Style Advance Audio Player Playlist
+    msaapPlaylist: Track[] = [
+      {
+          title: 'A Lesson Worth Learnin (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/rechargedEP/aLessonWorthLearnin.mp3'
+      },
+      {
+          title: 'Wing Nut (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/rechargedEP/wingNut.mp3'
+      },
+      {
+          title: 'Recharged (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/rechargedEP/recharged.mp3'
+      }
+  ];
 
-  ngOnInit(): void {
-  }
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = false;
+  msaapPageSizeOptions = 4;
+  msaapDisplayVolumeControls = true;
 
 }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,6 +5,22 @@ import { ReleaseContainerComponent } from './music/releases/release-container.co
 import { GTA19Component } from './music/releases/release-details/gta019/gta019.component';
 import { GTA18Component } from './music/releases/release-details/gta018/gta018.component';
 import { GTA17Component } from './music/releases/release-details/gta017/gta017.component';
+import { GTA16Component } from './music/releases/release-details/gta016/gta016.component';
+import { GTA15Component } from './music/releases/release-details/gta015/gta015.component';
+import { GTA14Component } from './music/releases/release-details/gta014/gta014.component';
+import { GTA13Component } from './music/releases/release-details/gta013/gta013.component';
+import { GTA12Component } from './music/releases/release-details/gta012/gta012.component';
+import { GTA11Component } from './music/releases/release-details/gta011/gta011.component';
+import { GTA10Component } from './music/releases/release-details/gta010/gta010.component';
+import { GTA9Component } from './music/releases/release-details/gta009/gta009.component';
+import { GTA8Component } from './music/releases/release-details/gta008/gta008.component';
+import { GTA7Component } from './music/releases/release-details/gta007/gta007.component';
+import { GTA6Component } from './music/releases/release-details/gta006/gta006.component';
+import { GTA5Component } from './music/releases/release-details/gta005/gta005.component';
+import { GTA4Component } from './music/releases/release-details/gta004/gta004.component';
+import { GTA2Component } from './music/releases/release-details/gta002/gta002.component';
+import { GTA3Component } from './music/releases/release-details/gta003/gta003.component';
+import { GTA1Component } from './music/releases/release-details/gta001/gta001.component';
 
 export const appRoutes:Routes = [
     { path: '', component: MainComponent },
@@ -15,6 +31,22 @@ export const appRoutes:Routes = [
     { path: 'releases/gta19', component: GTA19Component },
     { path: 'releases/gta18', component: GTA18Component },
     { path: 'releases/gta17', component: GTA17Component },
+    { path: 'releases/gta16', component: GTA16Component },
+    { path: 'releases/gta15', component: GTA15Component },
+    { path: 'releases/gta14', component: GTA14Component },
+    { path: 'releases/gta13', component: GTA13Component },
+    { path: 'releases/gta12', component: GTA12Component },
+    { path: 'releases/gta11', component: GTA11Component },
+    { path: 'releases/gta10', component: GTA10Component },
+    { path: 'releases/gta09', component: GTA9Component },
+    { path: 'releases/gta08', component: GTA8Component },
+    { path: 'releases/gta07', component: GTA7Component },
+    { path: 'releases/gta06', component: GTA6Component },
+    { path: 'releases/gta05', component: GTA5Component },
+    { path: 'releases/gta04', component: GTA4Component },
+    { path: 'releases/gta03', component: GTA3Component },
+    { path: 'releases/gta02', component: GTA2Component },
+    { path: 'releases/gta01', component: GTA1Component },
     
     { path: '', redirectTo: '/home', pathMatch: 'full'}
 ]
