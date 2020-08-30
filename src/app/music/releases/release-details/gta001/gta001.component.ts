44d4afdfb89e409df4c33a951ecef6f6917e36d7
Gary Hutchin <garyscotthutchin@gmail.com>
2020-08-30 15:53:03 +0100

    Release service, release details updates
    New items added to release-service array objects
    
    Style changes to release-details components
    
    release-details components updated to pull data from release-service

--- src/app/music/releases/release-details/gta001/gta001.component.ts
+++ src/app/music/releases/release-details/gta001/gta001.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Track } from 'ngx-audio-player';
+import { ReleaseService } from '../../shared/releases.service';
 
 @Component({
   selector: 'gta1-details',
@@ -8,19 +9,32 @@ import { Track } from 'ngx-audio-player';
 })
 export class GTA1Component {
 
-    // Material Style Advance Audio Player Playlist
-    msaapPlaylist: Track[] = [
+  release:any
+
+  constructor(private releaseService:ReleaseService) {
+  }
+
+  ngOnInit() {
+      this.release = this.releaseService.getRelease('gta01')
+  }
+  
+  // Material Style Advance Audio Player Playlist
+  msaapPlaylist: Track[] = [
       {
-          title: 'A Lesson Worth Learnin (Original Mix) by Gary The Apprentice',
-          link: './assets/audio/samples/rechargedEP/aLessonWorthLearnin.mp3'
+          title: 'Mv (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/mv.mp3'
       },
       {
-          title: 'Wing Nut (Original Mix) by Gary The Apprentice',
-          link: './assets/audio/samples/rechargedEP/wingNut.mp3'
+          title: 'Grep (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/grep.mp3'
       },
       {
-          title: 'Recharged (Original Mix) by Gary The Apprentice',
-          link: './assets/audio/samples/rechargedEP/recharged.mp3'
+          title: 'TraceRoute (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/traceroute.mp3'
+      },
+      {
+          title: 'TraceRoute (DJ Ze MigL Remix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/tracerouteZeMigL.mp3'
       }
   ];
 
--- src/app/music/releases/release-details/gta015/gta015.component.ts
+++ src/app/music/releases/release-details/gta015/gta015.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReleaseService } from '../../shared/releases.service';
+import { Track } from 'ngx-audio-player';
 
 @Component({
   selector: 'gta15-details',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GTA15Component implements OnInit {
 
-  constructor() { }
+  release:any
 
-  ngOnInit(): void {
+  constructor(private releaseService:ReleaseService) {
   }
 
+  ngOnInit() {
+      this.release = this.releaseService.getRelease('gta15')
+  }
+  
+  // Material Style Advance Audio Player Playlist
+  msaapPlaylist: Track[] = [
+      {
+          title: 'Mv (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/mv.mp3'
+      },
+      {
+          title: 'Grep (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/grep.mp3'
+      },
+      {
+          title: 'TraceRoute (Original Mix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/traceroute.mp3'
+      },
+      {
+          title: 'TraceRoute (DJ Ze MigL Remix) by Gary The Apprentice',
+          link: './assets/audio/samples/unixEP/tracerouteZeMigL.mp3'
+      }
+  ];
+
+  msaapDisplayTitle = true;
+  msaapDisplayPlayList = false;
+  msaapPageSizeOptions = 4;
+  msaapDisplayVolumeControls = true;
 }
