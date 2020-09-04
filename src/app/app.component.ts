e3c942a37f10d17a60fb18dfab382a9d1846f0b4
Gary Hutchin <garyscotthutchin@gmail.com>
2020-09-04 18:36:34 +0100

    Loading spinner added - various css changes
    Loading spinner added - various css changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationStart, NavigationEnd, Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gtadigital-app';
+
+  showLoading = true
+  constructor(private router: Router) {
+
+    this.router.events.subscribe((routerEvent: Event) => {
+      if(routerEvent instanceof NavigationStart) {
+        this.showLoading = true
+      }
+
+      if(routerEvent instanceof NavigationEnd) {
+        this.showLoading = false
+      }
+      
+    })
+  }
+
 }
