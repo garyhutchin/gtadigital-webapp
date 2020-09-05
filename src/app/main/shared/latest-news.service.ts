7c78c49f767d0b9919b87bf5abbfde16a03c0f40
Gary Hutchin <garyscotthutchin@gmail.com>
2020-09-05 19:07:23 +0100

    Various style and content changes
    Various style and content changes

--- src/app/main/shared/latest-news.service.ts
+++ src/app/main/shared/latest-news.service.ts
@@ -14,7 +14,7 @@ const NEWSITEMS = [
         info: 'The GTA Digital Podcast is back with the 30th installment. As always you can expect an hour of straight up, straight forward, hard banging techno. Tracks by the likes of Vincent de Wit, Mattias Fridell, Lars Klein & Concrete Djz. ',
         imageUrl: './assets/images/card-images/latest-news-images/podcast30.jpg',    
         internalUrl: '/music/podcasts/podcast-30',
-        internalUrlTitle: 'LISTEN NOW'    
+        internalUrlTitle: 'Listen Now'    
     },
     {
         title: 'GTA Digital Presents - September 2020',
@@ -28,20 +28,20 @@ const NEWSITEMS = [
         info: 'Podcast 29, you know the score, straight up banging techno. For this podcast I put together an hour set using tracks released on Armatura Clandestine. Some real solid, seriously banging hard techno. ',
         imageUrl: './assets/images/card-images/latest-news-images/podcast29.jpg',
         internalUrl: '/music/podcasts/podcast-29',
-        internalUrlTitle: 'LISTEN NOW'  
+        internalUrlTitle: 'Listen Now'  
     },
     {
         title: 'News Item 3',
         info: 'You can check out this podcast ',
         imageUrl: './assets/images/card-images/latest-news-images/podcast29.jpg',
         internalUrl: '/music/podcasts/podcast-29',
-        internalUrlTitle: 'LISTEN NOW'    
+        internalUrlTitle: 'Listen Now'    
     },
     {
         title: 'News Item 4',
         info: 'You can check out this podcast ',
         imageUrl: './assets/images/card-images/latest-news-images/podcast29.jpg' ,  
         internalUrl: '/music/podcasts/podcast-29',
-        internalUrlTitle: 'LISTEN NOW'  
+        internalUrlTitle: 'Listen Now'  
     },
 ]
