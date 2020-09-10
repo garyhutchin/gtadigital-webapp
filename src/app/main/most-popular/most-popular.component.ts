bd21ffba24b15ed0d30d81332de5d6deca695d9b
Gary Hutchin <garyscotthutchin@gmail.com>
2020-09-10 13:28:10 +0100

    Most pop component updated to include animations
    New CSS classes created to allow this to work, card structure css component updated with new classes and style rules

--- src/app/main/most-popular/most-popular.component.ts
+++ src/app/main/most-popular/most-popular.component.ts
@@ -9,21 +9,39 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
     styleUrls: ['../../css/card-structure.component.css'],
     animations: [
 
-        trigger('mostPopReleaseAnimation', [
+        trigger('mostPopReleaseAnim', [
   
           transition('* => *', [
   
             query(':enter', style({ opacity: 0}), {optional: true}),
   
             query(':enter', stagger('300ms', [
-              animate('1s 0.9s ease-in', keyframes([
+              animate('1s ease-in', keyframes([
                 style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
                 style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
                 style({opacity: 1, transform: 'translateY(0px)', offset: 1}),
               ]))
             ]))
   
-          ]),
+          ])
+ 
+        ]),
+
+        trigger('mostPopPodcastAnim', [
+  
+          transition('* => *', [
+  
+            query(':enter', style({ opacity: 0}), {optional: true}),
+  
+            query(':enter', stagger('300ms', [
+              animate('1s 0.5s ease-in', keyframes([
+                style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+                style({opacity: 1, transform: 'translateY(0px)', offset: 1}),
+              ]))
+            ]))
+  
+          ])
  
         ])
   
