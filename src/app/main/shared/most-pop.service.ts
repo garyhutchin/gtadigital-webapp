71691b2546384220bc2ed5cf9bf9ebf6481722ea
Gary Hutchin <garyscotthutchin@gmail.com>
2020-09-21 18:50:53 +0100

    services updated with relevant data from Firebase DB
    services updated with relevant data from Firebase DB
    
    Up to release service, release 04

--- src/app/main/shared/most-pop.service.ts
+++ src/app/main/shared/most-pop.service.ts
@@ -14,38 +14,38 @@ export class MostPopularService {
 
 const RELEASEITEMS = [
     {   /*Add latest release first*/
-        id: 20,
-        cardHeader: 'Latest Release',
-        title: 'Purist EP',
-        artistName: 'Ungrateful Ambassador',
-        catNo: '[GTA020]',
-        imageUrl: './assets/images/card-images/most-popular-images/newRelease.jpg'
+        artistName : "Ungrateful Ambassador",
+        cardHeader : "Latest Release",
+        catNo : "[GTA020]",
+        id : 20,
+        imageUrl : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Fcard-images%2Fmost-popular-images%2FnewRelease.jpg?alt=media&token=5e21a4e2-66d1-4b21-b165-f84a9e435b7f",
+        title : "Purist EP"
     },
     {
-        id: 0o2,
-        cardHeader: 'Most Popular Release',
-        title: 'The Brass Continent',
-        artistName: 'Jamie Ball',
-        catNo: '[GTA002]',
-        imageUrl: './assets/images/card-images/most-popular-images/mostPopRelease.jpg'
+        artistName : "Jamie Ball",
+        cardHeader : "Most Popular Release",
+        catNo : "[GTA002]",
+        id : 2,
+        imageUrl : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Fcard-images%2Fmost-popular-images%2FmostPopRelease.jpg?alt=media&token=65256437-0802-4467-a824-0300193e8322",
+        title : "The Brass Continent"
     }
 ]
     
 const PODCASTITEMS = [
     {
-        id: 30,
-        cardHeader: 'Latest Podcast',
-        title: 'GTA Digital Podcast #30',
-        artistName: 'mixed by Gary The Apprentice',
-        musicStyle: 'Style: Hard Techno',
-        imageUrl: './assets/images/card-images/most-popular-images/newPodcast.jpg'
+        artistName : "mixed by Gary The Apprentice",
+        cardHeader : "Latest Podcast",
+        id : "30",
+        imageUrl : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Fcard-images%2Fmost-popular-images%2FnewPodcast.jpg?alt=media&token=510e927a-cb16-4f9f-bc83-30d9dc6ce0cf",
+        musicStyle : "Style: Hard Techno",
+        title : "GTA Digital Podcast #30"
     },
     {
-        id: 7,
-        cardHeader: 'Most Popular Podcast',
-        title: 'GTA Digital Podcast #7',
-        artistName: 'mixed by Gary The Apprentice',
-        musicStyle: 'Style: Chunky, Hypnotic Techno',
-        imageUrl: './assets/images/card-images/most-popular-images/mostPopPodcast.jpg'
+        artistName : "mixed by Gary The Apprentice",
+        cardHeader : "Most Popular Podcast",
+        id : "7",
+        imageUrl : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Fcard-images%2Fmost-popular-images%2FmostPopPodcast.jpg?alt=media&token=16981a17-1402-4daf-9c2d-5f17d5128d41",
+        musicStyle : "Style: Chunky, Hypnotic Techno",
+        title : "GTA Digital Podcast #7"
     }
 ]
--- src/app/shared/content.service.ts
+++ src/app/shared/content.service.ts
@@ -53,34 +53,34 @@ const NAVITEMS = [
 const MAINCONTENT = [
     {
         id: 'home',
-        heroHeader: 'Underground Techno',
-        heroImage: '../../assets/images/logo.png',
-        heroText: 'GTA Digital started in 2013 as a platform for Gary The Apprentice to release his own music. Since then the label has grown, with the addition of a podcast series and more recently a monthly show on Fnoob Techno Radio',
-        heroButton: 'Listen Now',
-        header1: 'Latest News',
-        header2: 'GTA Digital Live',
-        gtaDigitalLiveHeader: 'GTA Digital Live Presents',
-        gtaDigitalLiveText: 'Tune into the GTA Digital Live stream to check out Gary The Apprentice playing all things techno, in full HD and crystal clear audio.</p><p>Head over to the live stream archive to check out recordings of all past streams.</p><p>Click the subscribe button below to stay notified.',
-        gtaDigitalLiveSrc: 'https://www.youtube.com/embed/6xTieGgxjDQ?modestbranding=1;controls=1;showinfo=0;rel=0;fs=1'
+        gtaDigitalLiveHeader : "GTA Digital Live Presents",
+        gtaDigitalLiveSrc : "https://www.youtube.com/embed/6xTieGgxjDQ?modestbranding=1;controls=1;showinfo=0;rel=0;fs=1",
+        gtaDigitalLiveText : "Tune into the GTA Digital Live stream to check out Gary The Apprentice playing all things techno, in full HD and crystal clear audio.</p><p>Head over to the live stream archive to check out recordings of all past streams.</p><p>Click the subscribe button below to stay notified.",
+        header1 : "Latest News",
+        header2 : "GTA Digital Live",
+        heroButton : "Listen Now",
+        heroHeader : "Underground Techno",
+        heroImage : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Flogo.png?alt=media&token=899bcdcb-0a2f-4927-8930-bb8e6b509ee1",
+        heroText : "GTA Digital started in 2013 as a platform for Gary The Apprentice to release his own music. Since then the label has grown, with the addition of a podcast series and more recently a monthly show on Fnoob Techno Radio"
     },
     {
-        id: 'about',
-        heroHeader: 'GTA Digital',
-        heroImage: '../../assets/images/logo.png',
-        heroText: 'Label, podcast series & radio show.<br>Releasing underground techno since 2013...',
-        aboutText: 'GTA Digital is a net label and Podcast series, set up by Gary The Apprentice, to provide the world with proper, underground Techno, staying clear of the more popular sound that is being churned out by bigger labels.</p><p>Here at GTA Digital, we are wanting to keep things real, releasing proper Techno, by proper Techno heads. We are always on the look out for new artists to get involved, so if you like what we are doing, and feel your music fits, please get in touch with a demo for us to consider releasing on the label.</p><p>This page will be used as our main platform for all of the Podcasts on the GTA Digital Podcast Series, if you would like to get involved, and provide a mix for the Podcast, please do get in touch.</p><p>To check out our full back catalogue of releases, to get information on the artists involved, and to check out the merchandise we have on sale.',
-        aboutImage: '../../assets/images/medium-images/aboutMe-header.jpg'
+        id: "about",
+        aboutImage : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2FaboutMe-header.jpg?alt=media&token=056609e0-72eb-4727-91a3-9e5150455c9b",
+        aboutText : "GTA Digital is a net label and Podcast series, set up by Gary The Apprentice, to provide the world with proper, underground Techno, staying clear of the more popular sound that is being churned out by bigger labels.</p><p>Here at GTA Digital, we are wanting to keep things real, releasing proper Techno, by proper Techno heads. We are always on the look out for new artists to get involved, so if you like what we are doing, and feel your music fits, please get in touch with a demo for us to consider releasing on the label.</p><p>This page will be used as our main platform for all of the Podcasts on the GTA Digital Podcast Series, if you would like to get involved, and provide a mix for the Podcast, please do get in touch.</p><p>To check out our full back catalogue of releases, to get information on the artists involved, and to check out the merchandise we have on sale.",
+        heroHeader : "GTA Digital",
+        heroImage : "https://firebasestorage.googleapis.com/v0/b/gta-digital-web-app.appspot.com/o/main-content%2Flogo.png?alt=media&token=899bcdcb-0a2f-4927-8930-bb8e6b509ee1",
+        heroText : "Label, podcast series & radio show.<br>Releasing underground techno since 2013..."
     },
     {
         id: 'music',
-        heroHeader: 'Music',
-        heroText: 'Check out all of the music on GTA Digital, releases, podcasts and also recordings from past live streams...',
-        heroButton: 'Check out the latest release',
-        header1: 'Releases',
-        header2: 'Podcasts',
-        header3: 'GTA Digital Live Archive',
-        button1: 'View All Releases',
-        button2: 'View All Podcasts'
+        button1 : "View All Releases",
+        button2 : "View All Podcasts",
+        header1 : "Releases",
+        header2 : "Podcasts",
+        header3 : "GTA Digital Live Archive",
+        heroButton : "Check out the latest release",
+        heroHeader : "Music",
+        heroText : "Check out all of the music on GTA Digital, releases, podcasts and also recordings from past live streams..."
     },
     {
         id: "under-construction",
