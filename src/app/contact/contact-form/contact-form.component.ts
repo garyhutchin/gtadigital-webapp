ea582e836a010502c285643d439f893365ce79f2
Gary Hutchin <garyscotthutchin@gmail.com>
2020-09-24 07:48:25 +0100

    placeholder added to name field of form
    placeholder added to name field of form

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-
+ 
   name: string;
   email: string;
   message: string;
